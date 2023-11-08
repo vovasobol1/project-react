6ee58834d3f79698007c8680a9baeb4659ed47aa
vovasobol1 <dengyrostov@gmail.com>
2023-11-08 14:21:59 +0300

    фунцкия фильтарции теперь работает

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,23 +2,23 @@ import React, {useState} from 'react';
 import './App.css';
 import {TaskType, Todolist} from "./Todolist";
 
-export type filterValuesType = 'all' | 'comleted' |'active'  //тип значений для фильтрации чтобы в качестве фильтра не была любая строка
+export type FilterValuesType = 'all' | 'completed' |'active'  //тип значений для фильтрации чтобы в качестве фильтра не была любая строка
 
     function App() {
-    let [tasks ,setTasks ] = useState( [
+    let [tasks ,setTasks ] = useState<Array<TaskType>>( [
         {id: 1, title: 'css', isDone: true},
         {id: 2, title: 'js', isDone: true},
         {id: 3, title: 'react', isDone: false}
     ])
+    let [filter ,setFilter ] = useState<FilterValuesType>("all")
 
-
-    let [filter ,setFilter ] = useState<filterValuesType>("completed")
-
+    function changeFilter(value : FilterValuesType){
+        //функция которая будет менять фильтр
+        setFilter(value)
+    }
     function deleteTask(id: number) {
         let filteredTasks = tasks.filter(task => id !== task.id)// если это не то дело которое нужно удалить то вернется true и оно не удалится
         setTasks(filteredTasks)
-
-        console.log(tasks)
     }
 
     let TasksForTodolist = tasks
@@ -36,6 +36,7 @@ export type filterValuesType = 'all' | 'comleted' |'active'  //тип значе
                 title="what to learn" //передаем заголовок туду листа
                 tasks={TasksForTodolist}         //передаем массив дел
                 deleteTask={deleteTask} //передаем функцию удаления одного дела
+                changeFilter={changeFilter} //передаем функцию которая менеят фильтры
             />
         </div>
     );
--- my-app/src/Todolist.tsx
+++ my-app/src/Todolist.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {Simulate} from "react-dom/test-utils";
 import click = Simulate.click;
+import {FilterValuesType} from "./App";
+
 
 export type TaskType = {
     id: number
@@ -11,7 +13,8 @@ export type TaskType = {
 type PropsType = {
     title: string
     tasks: Array<TaskType> //мы ждем на вход массив состоящий из обьектов TaskType
-    deleteTask : Function
+    deleteTask : (id : number) => void
+    changeFilter: (value: FilterValuesType) => void
 }
 
 export function Todolist(props: PropsType) {
@@ -36,9 +39,9 @@ export function Todolist(props: PropsType) {
                 }
             </ul>
             <div>
-                <button>all</button>
-                <button>active</button>
-                <button>completed</button>
+                <button onClick={()=>{props.changeFilter('all')}} >all</button>
+                <button onClick={()=>{props.changeFilter('active')}} >active</button>
+                <button onClick={()=>{props.changeFilter('completed')}} >completed</button>
             </div>
         </div>
     )
