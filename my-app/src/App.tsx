6fb4c9c489e14b2b815d1b871186c545c1b94db9
vovasobol1 <dengyrostov@gmail.com>
2023-11-08 13:50:27 +0300

    useState переписан в более простой вид

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,15 +3,12 @@ import './App.css';
 import {TaskType, Todolist} from "./Todolist";
 
 function App() {
-    let initTasks: Array<TaskType> = [
+    let [tasks ,setTasks ] = useState( [
         {id: 1, title: 'css', isDone: true},
         {id: 2, title: 'js', isDone: true},
         {id: 3, title: 'react', isDone: false}
-    ]
+    ])
 
-    let arr = useState(initTasks) ;
-    let tasks = arr[0]
-    let setTasks = arr[1]
 
     function deleteTask(id: number) {
         let filteredTasks = tasks.filter(task => id !== task.id)// если это не то дело которое нужно удалить то вернется true и оно не удалится
